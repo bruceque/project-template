d764fa43332c5ecc9f576cc6967b3b9e326c7089
qiuxiaoquan <xiaoquan.qiu@tqmall.com>
2016-07-25 09:02:52 +0800

    servlet-api 2.5删除依赖

--- lightning-web/src/test/java/test/ZeusServer.java
+++ lightning-web/src/test/java/test/ZeusServer.java
@@ -9,10 +9,10 @@ public class ZeusServer {
 
         WebAppContext context = new WebAppContext();
         context.setContextPath("/");
-        context.setDescriptor("./venus-web/src/main/webapp/WEB-INF/web.xml");
-        context.setResourceBase("./venus-web/src/main/webapp");
+        context.setDescriptor("./lightning-web/src/main/webapp/WEB-INF/web.xml");
+        context.setResourceBase("./lightning-web/src/main/webapp");
         //解决静态资源缓存后再ide里面不能修改问题
-        context.setDefaultsDescriptor("./venus-web/src/test/resources/webdefault.xml");
+        context.setDefaultsDescriptor("./lightning-web/src/test/resources/webdefault.xml");
         context.setParentLoaderPriority(true);
         server.setHandler(context);
 
